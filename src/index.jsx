42ff5e94c55ca6126d9bbfc6a4e92075a119e6e4
Gregory Lock <Gregory.Lock@outfoxthemarket.co.uk>
2023-08-09 14:48:56 +0100

    Added protected route

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom' 
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
+import AuthRequired from './components/AuthRequired.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,10 +12,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <header>
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
+                <Link to="/protected">Protected</Link>
             </header>
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route element={<AuthRequired />}>
+                    <Route path="/protected" element={<h1>Secret Page</h1>} />
+                </Route>
             </Routes>
         </div>
     </BrowserRouter>
