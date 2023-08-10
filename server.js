9b587c7f11c39fcadcb485a8501129f0e1d7442d
Gregory Lock <Gregory.Lock@outfoxthemarket.co.uk>
2023-08-10 09:29:39 +0100

    Added form functionality

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
+// Install required packages
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-console.log('Mongoose version : ', mongoose.version);
-
+// Set express to app for quick use
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Inlcude body parser
+app.use(bodyParser.json());
+
 // Serve static files from the Vite build output
 app.use(express.static(path.join(__dirname, 'dist')));
+
+// Test use of api for submitting form data
+app.post('/api/submit-form', (req, res) => {
+    console.log("Data")
+    const formData = req.body;
+    console.log(formData);
+});
 
 // Route to serve your React app
 app.get('*', (req, res) => {
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
 import '../assets/styles/login.css'
 
 export default function Register() {
+  const [formData, setFormData] = useState({
+    username: 'Jim ',
+    password: 'Password'
+  });
+
+  const handleSubmit = async (error) => {
+    try {
+      const response = await fetch('/api/submit-form', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify("Hello")
+      })
+    } catch (error) {
+      console.log('Error submitting form', error)
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="register-form-container">
       <h2>REGISTER</h2>
-        <form action="">
-          <label>
-            <input type="text" />
-          </label>
+      <form className="register-form" onSubmit={handleSubmit}>
+          <input type="text" value={formData.name} placeholder="Username"/>
+          <input type="text" placeholder="Confirm Username"/>
+          <input type="text" placeholder="Password"/>
+          <input type="text" placeholder="Confirm Password"/>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
