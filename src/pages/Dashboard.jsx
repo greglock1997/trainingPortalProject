87d4d5d40a2afefc06360cbd444e1ab367e01216
Gregory Lock <Gregory.Lock@outfoxthemarket.co.uk>
2023-08-29 13:19:20 +0100

    Moved dashboard styles to seperate stylesheet

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom' 
 import axios from 'axios';
 
+import dashboardStyles from '../assets/styles/dashboard.module.css'
+
 export default function Dashboard() {
     const [completedUnits, setCompletedUnits] = useState([]);
 
@@ -22,31 +24,31 @@ export default function Dashboard() {
     const isCompleted = (unitNumber)  => {
         if (unitNumber === 1 || completedUnits.includes(unitNumber - 1)) {
             if (completedUnits.includes(unitNumber)) {
-                return 'unit-link-completed';
+                return dashboardStyles['unit-link-completed'];
             } else {
-                return 'unit-link'
+                return dashboardStyles['unit-link'];
             }
         } else {
-            return 'unit-link-locked';
+            return dashboardStyles['unit-link-locked'];
         }
     };
 
     return (
-        <div className="dashboard-container">
-            <div className="progress-bar-container">
-                <div className="progress-bar" style={{width: `${window.innerWidth * (completedUnits.length / 9 )}px`}}></div>
+        <div className={dashboardStyles['dashboard-container']}>
+            <div className={dashboardStyles['progress-bar-container']}>
+                <div className={dashboardStyles['progress-bar']} style={{width: `${window.innerWidth * (completedUnits.length / 9 )}px`}}></div>
             </div>
-            <div className="unit-section">
+            <div className={dashboardStyles['unit-section']}>
                 <Link className={isCompleted(1)} to="/unit/1">Intro</Link>
                 <Link className={isCompleted(2)} to="/unit/2">The UK Energy Industry & Switching</Link>
                 <Link className={isCompleted(3)} to="/unit/3">Introduction to Energy Bills</Link>
             </div>
-            <div className="unit-section">
+            <div className={dashboardStyles['unit-section']}>
                 <Link className={isCompleted(4)} to="/unit/4">Energy Price Gaurantee</Link>
                 <Link className={isCompleted(5)} to="/unit/5">Metering</Link>
                 <Link className={isCompleted(6)} to="/unit/6">Customer Vulnerabilities</Link>
             </div>
-            <div className="unit-section">
+            <div className={dashboardStyles['unit-section']}>
                 <Link className={isCompleted(7)} to="/unit/7">Ability to Pay, Debt and Payment Plans</Link>
                 <Link className={isCompleted(8)} to="/unit/8">Fuel Direct</Link>
                 <Link className={isCompleted(9)} to="/unit/9">Complaints</Link>
